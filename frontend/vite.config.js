5fab17a9b148511f55786967a4f57602a6bf138d
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-09-09 17:25:00 +0100

    new files added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api/": "http://localhost:5000",
@@ -11,3 +11,4 @@ export default defineConfig({
     },
   },
 });
+
